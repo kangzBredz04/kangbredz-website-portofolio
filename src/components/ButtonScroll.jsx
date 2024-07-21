f67ac487c5745cac9c796f76e8828eb7063e3669
wahyu <wabredz1234@gmail.com>
2024-07-21 20:58:48 +0700

    update pada komponen scroll

--- src/components/ButtonScroll.jsx
+++ src/components/ButtonScroll.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useEffect } from "react";
 import { RxPinTop } from "react-icons/rx";
 
 function ButtonScroll({ scrollRef }) {
@@ -9,8 +10,23 @@ function ButtonScroll({ scrollRef }) {
       behavior: "smooth",
     });
   };
+
   return (
     <div
+      onScroll={useEffect(() => {
+        window.addEventListener("scroll", () => {
+          const scrollTop =
+            window.pageYOffset || document.documentElement.scrollTop;
+          console.log("Scroll Top:", scrollTop);
+        });
+        () => {
+          window.removeEventListener("scroll", () => {
+            const scrollTop =
+              window.pageYOffset || document.documentElement.scrollTop;
+            // console.log("Scroll Top:", scrollTop);
+          });
+        };
+      }, [])}
       className="sticky top-3/4 left-full z-20 mr-5 -mb-12  w-12 h-12 bg-[#7C96AB] p-4 rounded-full cursor-pointer"
       onClick={() => {
         handleScroll(scrollRef.current);
