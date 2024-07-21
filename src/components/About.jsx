66cee262fa8f18e8cbf8636ac4d4f8be5fb3c7e0
wahyu <wabredz1234@gmail.com>
2024-07-21 20:58:11 +0700

    update tampilan komponen about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,17 +6,24 @@ export default function About() {
   const { theme, setTheme } = useContext(ThemeContext);
   return (
     <div
-      className={`px-7 md:px-10 ${
+      className={`px-10 md:px-10 ${
         theme === "dark" ? "bg-[#1a1a29] text-white" : "bg-white text-black"
       } mb-9`}
       id="about"
     >
-      <h1 className="text-3xl  text-primary font-semibold">About Me</h1>
-      <p className="md:w-2/3 leading-[2]">
+      <h1 className="text-3xl text-primary font-semibold">About Me</h1>
+      <p className="text-justify leading-7 ">
         Hi, my name is Kang Bred'z , i am a Fullstack web developer, UI
         designer, and Mobile developer. I have honed my skills in Web
         Development and advance i have core understanding of advance UI design
-        principles. Here are the major skiills i have.
+        principles. Here are the major skiills i have. Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Pariatur error nesciunt qui dicta
+        itaque corrupti obcaecati nihil laboriosam non? Quisquam quod officia
+        repellendus, numquam error quam quia est vero illo? A inventore
+        repudiandae corporis facilis! Deserunt eveniet velit similique
+        accusantium quos, enim dignissimos quae in labore eaque saepe!
+        Asperiores iste dicta voluptate non consequatur architecto quos totam
+        dolor quod aspernatur.
       </p>
     </div>
   );
